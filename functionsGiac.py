3427542761dd2e3be29e37389a853e1c8c1f038c
Giacomo Maretto <giacomomaretto@gmail.com>
2018-01-03 16:17:46 +0100

    es1.1

---
+++ functionsGiac.py
@@ -0,0 +1,38 @@
+import numpy as np
+from scipy.optimize import minimize
+
+def gauss_kernel(x_i, x_j, gamma):
+    '''RBF kernel function, gamma hyperparameter'''
+    return np.exp(-1*gamma*np.linalg.norm(x_i-x_j)**2)
+
+def poly_kernel(x_i, x_j, p):
+    '''Polynomial kernel functionn, p hyperparameter'''
+    if p < 1:
+        print('Invalid value for parameter p')
+        return
+    return (np.dot(x_i, x_j) + 1)**p
+
+def Q_matrix(X, y, gamma):
+    L = X.shape[0]
+    Q_temp = np.ones((L,L))
+
+    for i in range(L):
+        for j in range(i, L):
+            Q_temp[i, j] = y[i] * y[j] * gauss_kernel(X[i], X[j], gamma)
+    Q = 0.5 * (Q_temp + Q_temp.T)
+    return Q 
+
+def objective(alpha, Q):
+    L = Q.shape[0]
+    
+    q = 0.5*np.dot(np.dot(alpha.T, Q), alpha)
+    
+    e = np.ones((L,1))
+    s = np.dot(e.T, alpha)
+    
+    return (q - s)
+
+
+def constraint1(y,alpha):
+    return np.dot(y.T,alpha)
+
---
+++ main.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from functionsGiac import *
+
+# Importing dataset
+dataset = pd.read_csv('2017.12.11 Dataset Project 2.csv', header = 0, sep = ',')
+X = dataset.iloc[:, :-1].values
+y = 2*(dataset.iloc[:, -1].values.reshape(-1,1)) - 1 # Mapping {0, 1} to {-1, 1}
+
+# Split train, validation, test set
+X_train_n_validation, X_test, y_train_n_validation, y_test = train_test_split(X, y, test_size = 0.3, random_state = 1733715)
+
+X_train, X_validation, y_train, y_validation = train_test_split(X_train_n_validation, y_train_n_validation, test_size = 0.3, random_state = 1733715)
+
+# Q matrix of the dual problem
+Q = Q_matrix(X_train, y_train, 1)
+
+# Inintialize vector of Lagrage multipliers
+L = X_train.shape[0]
+alpha = np.zeros((L, 1))
+
+print(objective(alpha, Q))
+
+
+# initial guesses
+n = len(X_train)
+x0 = np.zeros((n,1))
+
+# show initial objective
+print('Initial Objective: ' + str(objective(x0, Q)))
+
+'''
+GRIDSEARCH C,GAM
+'''
+# optimize
+C= 2
+b = (0, C)
+bnds = tuple(b for i in range(n))
+con = lambda alpha: constraint1(y_train, alpha)
+con1 = {'type': 'eq', 'fun': con}
+cons = ([con1])
+solution = minimize(objective, x0, args = (Q), method='SLSQP',
+                    bounds=bnds, constraints=cons)
+x = solution.x
+
+# show final objective
+print('Final Objective: ' + str(objective(x, Q)))
+
+# print solution
+print('Solution')
+
